49d7c6b75b3cc0e3d3b1ded2e2522e909b72e481
Nathalia Pereira <nathalia.pereira252000@gmail.com>
2021-06-09 16:10:15 -0300

    feat: Torna o botão de voltar funcional

--- src/pages/Visualizacao Post/index.js
+++ src/pages/Visualizacao Post/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import LeftArrow from '../../assets/Icon awesome-chevron-left.svg';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
@@ -8,24 +9,31 @@ import Cadastro from '../../assets/Cadastro.jpg';
 import * as Styled from './styles'
 
 const VisualizacaoPost = () => {
-return (
-    <div>
-    <Navbar />
-    <Styled.Page>
-        <Styled.BackButton>
-            <img src={LeftArrow} />
-            <Styled.Back>Voltar</Styled.Back>
-        </Styled.BackButton>
-        <Styled.Title>Um título legal</Styled.Title>
-        <Styled.Date>Data de postagem</Styled.Date>
-        <Styled.Banner src={Cadastro}></Styled.Banner>
-        <Styled.Body>
-            <p>Exemplo de postagem</p>
-        </Styled.Body>
-    </Styled.Page>
-    <Footer />
-    </div>
-);
+
+    let history = useHistory();
+
+    const redirect = () => {
+      history.push('/')
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <Styled.Page>
+                <Styled.BackButton onClick={redirect}>
+                    <img src={LeftArrow}/>
+                    <Styled.Back>Voltar</Styled.Back>
+                </Styled.BackButton>
+                <Styled.Title>Um título legal</Styled.Title>
+                <Styled.Date>Data de postagem</Styled.Date>
+                <Styled.Banner src={Cadastro}></Styled.Banner>
+                <Styled.Body>
+                    <p>Exemplo de postagem</p>
+                </Styled.Body>
+            </Styled.Page>
+            <Footer />
+        </div>
+    );
 }
 
 export default VisualizacaoPost;
