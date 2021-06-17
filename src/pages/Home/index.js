1b85c1e578d49e064904c1eb6e765cee7bed75f1
Gustavo <gustavosports7@gmail.com>
2021-06-17 02:41:43 -0300

    :sparkles: feat: Componente sobre a autora home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,43 @@
-import React from 'react'
-import {Background} from './styles'
-import {Titulo} from './styles'
+import React from 'react';
+import * as styled from './styles';
 
+import TwitterIcon from './Twitter.svg';
+import FaceIcon from './Facebook.svg';
+import InstaIcon from './Instagram.svg';
+import SearchIcon from './SearchIcon.svg';
+import Cintia from './Cintia.jpg'
 
-function index() {
+import InputText from '../../components/Contact Input'
+
+function Home() {
     return (
-        <Background>
-            <Titulo>
-               Home 
-            </Titulo>
-        </Background>
+        <styled.Background>
+            <styled.LeftBox>
+                <styled.PhotoContainer>
+                    <img src={Cintia} class='Foto'/>
+                    <styled.TextoContainer>
+                        <styled.Title> SOBRE A AUTORA </styled.Title>
+                        <styled.Subtitle> Cíntia Lorenzzo </styled.Subtitle>
+                        <styled.Texto> 
+                            Sou veterinária há 5 anos,
+                            apaixonada pelo mundo animal,
+                            principalmente os que estão 
+                            sempre conosco no dia a dia.
+                            Quando não estou no meu 
+                            consultório ou com meus pets,
+                            estou aqui escrevendo conteúdo para vocês. Espero que você goste! 
+                        </styled.Texto>
+                    </styled.TextoContainer>
+                <styled.RedesContainers>
+                    <img src={TwitterIcon} class='Twitter'/>
+                    <img src={FaceIcon} class='Twitter'/>
+                    <img src={InstaIcon} class='Twitter'/>
+                </styled.RedesContainers>
+                </styled.PhotoContainer>
+            </styled.LeftBox>
+        </styled.Background>
+        
     )
 }
 
-export default index
+export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,16 +1,74 @@
 import styled from 'styled-components'
 
-export const Background = styled.section`
-width: 100%;
-height: 80vh;
+export const Background = styled.div`
+  display: flex;
+  margin: 50px auto 50px 50px;
+  padding: 0;
 `
-export const Titulo = styled.section`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 70vh;
-color: black;
-font-size: 50px;
-font-family: 'Montserrat', sans-serif;
+export const LeftBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 400px;
+`
 
+export const PhotoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  align-items: center;
+  background-color: #FBE9F6;
+    
+  img.Foto{
+    position: relative;
+    margin-top: -2rem;
+    object-fit: cover;
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+  }
+
+` 
+
+export const TextoContainer = styled.div`
+  display: flex;
+  font-family: 'MontSerrat', sans-serif;
+  width: 80%;
+  text-align: center;
+  margin: 0;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 1em;
 `
+
+export const Title = styled.a`
+  font-family: 'MontSerrat', sans-serif;
+  font-size: 0.9rem;
+  font-weight: 300;
+`
+
+export const Subtitle = styled.a`
+  font-family: 'MontSerrat', sans-serif;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #C882B4;
+`
+
+export const Texto = styled.a`
+  font-family: 'MontSerrat', sans-serif;
+  font-weight: 350;
+  font-size: 0.87rem;
+  margin-top: 0.6rem;
+`
+
+export const RedesContainers = styled.div`
+  display: flex;
+  margin-top: 10px;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+export const SearchInput = styled.div`
+  
+`
+
